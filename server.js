7223f008275e405424ef816d630919ec13ab0181
Martin Lynx <martilve@tktk.ee>
2023-11-19 16:54:16 +0200

    second com

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = 3000;
 const booksRouter = require('./routes/books');
+const countriesRouter = require('./routes/countries');
 
 const app = express();
 
@@ -11,9 +12,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/books', booksRouter);
-
-
+app.use('/countries', countriesRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
 });
+
